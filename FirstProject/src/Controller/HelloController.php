0065f7e13d0c33b13fb8ca31e5f275e6df6f68d8
louayparadigma <mohamedlouay.logtari@esprit.tn>
2025-05-27 22:44:10 +0100

    hello world

---
+++ FirstProject/src/Controller/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HelloController extends AbstractController
+{
+    #[Route('/', name: 'app_hello')]
+    public function index(): Response
+    {
+        return new Response('<h1>Hello from Symfony!</h1>');
+    }
+}
