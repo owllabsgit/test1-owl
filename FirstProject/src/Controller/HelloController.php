d7d662baf7f8688aa25d59f28b53f5acb248978f
louayparadigma <mohamedlouay.logtari@esprit.tn>
2025-06-24 19:31:12 +0100

    owllabs

--- FirstProject/src/Controller/HelloController.php
+++ FirstProject/src/Controller/HelloController.php
@@ -11,6 +11,6 @@ class HelloController extends AbstractController
     #[Route('/', name: 'app_hello')]
     public function index(): Response
     {
-        return new Response('<h1>Hello from owl !</h1>');
+        return new Response('<h1>Hello from owl labs !</h1>');
     }
 }
