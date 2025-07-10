f63b74be6770f3637b48b3f7a557b7db5b215443
louayparadigma <mohamedlouay.logtari@esprit.tn>
2025-07-10 16:31:22 +0100

    changed cun

--- FirstProject/src/Controller/HelloController.php
+++ FirstProject/src/Controller/HelloController.php
@@ -11,6 +11,6 @@ class HelloController extends AbstractController
     #[Route('/', name: 'app_hello')]
     public function index(): Response
     {
-        return new Response('<h1>Hello from 42 creation !</h1>');
+        return new Response('<h1>Hello from cun !</h1>');
     }
 }
