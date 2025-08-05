2721ff73ddee9600bb643923ff3dd183dbb2775c
louayparadigma <106873768+louayparadigma@users.noreply.github.com>
2025-08-05 18:34:03 +0100

    Update HelloController.php
    webhook

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -11,6 +11,6 @@ class HelloController extends AbstractController
     #[Route('/', name: 'app_hello')]
     public function index(): Response
     {
-        return new Response('<h1>Hello from test3!</h1>');
+        return new Response('<h1>Hello from test213!</h1>');
     }
 }
