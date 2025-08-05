b3e0d4a406aec479179b13eceb205ddfc1bdd056
louayparadigma <106873768+louayparadigma@users.noreply.github.com>
2025-08-05 18:39:21 +0100

    Update HelloController.php
    130

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -11,6 +11,6 @@ class HelloController extends AbstractController
     #[Route('/', name: 'app_hello')]
     public function index(): Response
     {
-        return new Response('<h1>Hello from test121!</h1>');
+        return new Response('<h1>Hello from test130!</h1>');
     }
 }
