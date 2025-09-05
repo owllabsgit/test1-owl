ff404f0192655112c88dacbd407c915fb4db9abb
owllabsgit <github@owllabs.pro>
2025-09-05 14:42:39 +0100

    538

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -11,6 +11,6 @@ class HelloController extends AbstractController
     #[Route('/', name: 'app_hello')]
     public function index(): Response
     {
-        return new Response('<h1>Hello from 537! </h1>');
+        return new Response('<h1>Hello from 538! </h1>');
     }
 }
